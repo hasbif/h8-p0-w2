7def7f84d12d9543c753059eb7de328d04fd01ad
hasbif <hasbisenpai@gmail.com>
2020-01-31 15:05:07 +0700

    update 2 8 and 13

--- exercise-13.js
+++ exercise-13.js
@@ -1,7 +1,21 @@
 function xo(str) {
     // you can only write your code here!
-    for(var i = 0; i<str.length; i++)
-    if 
+    var countx = 0
+    var county = 0
+    for(var i = 0; i<str.length; i++){
+
+
+      if  (str[i] == 'x'){
+        countx +=1
+      }else county +=1
+
+    }
+
+
+
+    if (county == countx){
+      return(true)
+    }else return (false)
   }
   
   // TEST CASES
@@ -9,4 +23,4 @@ function xo(str) {
   console.log(xo('oxooxo')); // false
   console.log(xo('oxo')); // false
   console.log(xo('xxxooo')); // true
-  console.log(xo('xoxooxxo')); // true
+  console.log(xo('xoxooxxo')); //true
